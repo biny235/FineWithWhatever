9b0dae1f9335f061c482e752f1bb35d7f4c7ed64
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-05-27 11:51:20 -0400

    built users

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/fww');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/fww', {logging: false});
 
 module.exports = conn;
--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,26 @@ const User = require('./User');
 
 const syncAndSeed = ()=>{
   return conn.sync({ force: true })
+    .then(()=>{
+      return Promise.all([
+        User.create({username: 'test'}),
+        User.create({username: 'test'}),
+        User.create({username: 'test'}),
+        User.create({username: 'test'}),
+        User.create({username: 'test'})
+      ])
+    })
+    .then(users => {
+      const id =  users[0].id;
+      users.forEach(user => {
+        user.addFriend(users[0])
+      })
+    })
 }
+
+User.belongsToMany(User, { as: 'friend', through: 'friends' })
+
+
 
 module.exports = {
   syncAndSeed,
