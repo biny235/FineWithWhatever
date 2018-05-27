3bf31a1dcd39b5bd86005334ddef521a7f231a5a
Alan Fleming <alalfleming@gmail.com>
2018-05-27 11:58:51 -0400

    started assocations

--- server/db/User.js
+++ server/db/User.js
@@ -14,6 +14,6 @@ const User = conn.define('user', {
   email:{
     type: Sequelize.STRING
   }
-})
+});
 
 module.exports = User;
--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,24 @@
 const conn = require('./conn');
 const User = require('./User');
+const Place = require('./Place');
+const Plan = require('./Plan');
+const Group = require('./Group');
 
+Place.belongsTo(Plan);
+Plan.hasMany(Place);
+User.belongsTo(Group);
+Group.hasMany(User);
 
 const syncAndSeed = ()=>{
-  return conn.sync({ force: true })
-}
+  return conn.sync({ force: true });
+};
 
 module.exports = {
   syncAndSeed,
   models:{
-    User
+    User,
+    Group,
+    Plan,
+    Place
   }
-}
+};
