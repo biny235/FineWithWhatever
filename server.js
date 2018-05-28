353f428178bdfd497cd8b61e752bee49ddd80a55
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-05-28 16:30:38 -0400

    basic sockets working

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const { syncAndSeed } = require('./server/db');
-const server = require('http').createServer(require('./server/app'));
+const socketio = require("socket.io")
+const _server = require('http').createServer(require('./server/app'));
 
 const port = process.env.PORT || 3000;
 
 syncAndSeed()
-  .then(() => {
-    server.listen( port, ()=> console.log(`Listening on port ${port}`));
-  });
+
+const server = _server.listen( port, ()=> console.log(`Listening on port ${port}`));
+const io = socketio(server);
+
+let counter = 0
+
+io.on('connection', (socket)=>{
+  
+  socket.on('disconnect', () => console.log("Goodbye"))
+  socket.on('add', count => {
+    counter = count
+    socket.broadcast.emit('setCounter', counter)
+  })
+  socket.emit('setCounter', counter)
+
+})
+
 
