c5bfaf091ab1f50f87da51b74b5dbd0608a17ebe
Alan Fleming <alalfleming@gmail.com>
2018-05-27 20:42:50 -0400

    auth finished

--- server/db/User.js
+++ server/db/User.js
@@ -45,14 +45,15 @@ User.exchangeTokenForUser = function (token) {
     const id = jwt.decode(token, KEY).id;
     return User.findById(id)
       .then(user => {
-        if (user)
+        if (user) {
           return user;
+        }
         throw { status: 401 };
       })
-      .catch(()=> {throw {status: 401};});
+      .catch(() => { throw { status: 401 }; });
   }
   catch (ex) {
-    return Promise.reject({ throw: 401 });
+    return Promise.reject({ status: 401 });
   }
 };
 
