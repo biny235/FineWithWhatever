766346bcbb160e9c7b61ae73f437e80f21527f98
Alan Fleming <alalfleming@gmail.com>
2018-05-28 14:12:36 -0400

    separated out the routes from the server into appjs

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
-const express = require('express');
-const app = express();
-const volleyball = require('volleyball');
-const path = require('path');
-
-app.use(volleyball);
-app.use(require('body-parser').json());
-app.use(require('body-parser').urlencoded({ extended: true }));
-
-app.use('/vendor', express.static(path.join(__dirname, '/node_modules')));
-app.use('/dist', express.static(path.join(__dirname, '/dist')));
-
-app.use('/api', require('./server/routes/api'));
-app.use('/auth', require('./server/routes/auth'));
-
-// app.get('/', (req, res, next) => {
-//   res.sendFile(path.join(__dirname, './index.html'));
-// });
-
-app.use((err, req, res, next) => {
-  console.log(`*** There is an error! ${err.stack} ***`);
-});
+const { syncAndSeed } = require('./server/db');
+const server = require('http').createServer(require('./server/app'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
+syncAndSeed()
+  .then(() => {
+    server.listen( port, ()=> console.log(`Listening on port ${port}`));
+  });
 
