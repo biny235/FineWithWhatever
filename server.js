c5193bda1c3c3a3dfc51503e3be6d47b25c7129e
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-03 12:24:12 -0400

    commit

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const _server = require('http').createServer(require('./server/app'));
 
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-syncAndSeed()
-=======
- syncAndSeed();
->>>>>>> master
+// syncAndSeed()
 
 const server = _server.listen( port, ()=> console.log(`Listening on port ${port}`));
 const io = socketio(server);
--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,7 @@ const conn = require('./conn');
 const User = require('./User');
 const Place = require('./Place');
 const Plan = require('./Plan');
+const Favorite = require('./Favorite');
 
 
 
@@ -9,40 +10,31 @@ const syncAndSeed = () => {
   return conn.sync({ force: true })
     .then(() => {
       return Promise.all([
-<<<<<<< HEAD
         User.create({username: 'Moe', password: 'MOE'}),
         User.create({username: 'Larry', password: 'LARRY'}),
         User.create({username: 'Curly', password: 'CURLY'}),
         User.create({username: 'test', password: '123'}),
-=======
-        User.create({ username: 'Moe', password: 'MOE' }),
-        User.create({ username: 'Larry', password: 'LARRY' }),
-        User.create({ username: 'Curly', password: 'CURLY' }),
-        User.create({ username: 'test' }),
-        User.create({ username: 'test' })
->>>>>>> master
       ])
     })
     .then(users => {
-      const id = users[0].id;
-      users.forEach(user => {
-        user.addFriend(users[0]);
-      });
+      users.forEach(user =>{
+        users[0].addFriend(user);
+      })
+      users.forEach(user =>{
+        users[1].addFriend(user);
+      }) 
     });
 };
 
 /* Friends */
 User.belongsToMany(User, { as: 'friends', through: 'friend' });
-<<<<<<< HEAD
+
 Plan.belongsTo(User);
 User.hasMany(Plan);
-Place.belongsToMany(Plan, { as: 'recommendation', through: 'recommendations' });
-Place.belongsToMany(User, {as: 'favorites', through: 'user_favorites'})
-=======
 
 /* Participants */
-User.belongsToMany(Plan, { through: 'participants' });
-Plan.belongsToMany(User, { through: 'participants' });
+// User.belongsToMany(Plan, { through: 'participants' });
+// Plan.belongsToMany(User, { through: 'participants' });
 
 /* Recommendations */
 Plan.belongsToMany(Place, { through: 'recommendations' });
@@ -54,12 +46,12 @@ Place.belongsToMany(Plan, { through: 'recommendations' });
 /* Favorites */
 User.belongsToMany(Place, { through: Favorite });
 Place.belongsToMany(User, { through: Favorite });
->>>>>>> master
 
 module.exports = {
   syncAndSeed,
   models: {
     User,
+    Favorite,
     Plan,
     Place
   }
