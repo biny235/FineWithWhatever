09ac55ef8a7ad2ebd70adc4921d54a32d2ec8601
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-05 20:32:39 -0400

    fixing seed

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const _server = require('http').createServer(require('./server/app'));
 const port = process.env.PORT || 3000;
 const server = _server.listen( port, ()=> console.log(`Listening on port ${port}`));
 
+// process.env.SYNC = true
+
 process.env.SYNC ? seed() : null;
 
 const io = socketio(server);
