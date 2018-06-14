96ea77369754bdf6deb2adfbc37d70d8491c2c3e
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-13 21:29:41 -0400

    sockets

--- server.js
+++ server.js
@@ -11,17 +11,19 @@ process.env.SYNC ? seed() : null;
 
 const io = socketio(server);
 
-let counter = 0
-
 io.on('connection', (socket) => {
-
   socket.on('disconnect', () => console.log("Goodbye"))
-  socket.on('add', count => {
-    counter = count
-    socket.broadcast.emit('setCounter', counter)
+  socket.emit('connected')
+  socket.on('broadcasting', (plan)=> {
+    console.log(plan)
+    socket.broadcast.emit('newBroadcast', plan )
   })
-  socket.emit('setCounter', counter)
-
+  socket.on('recommending', (recommendation)=> {
+    console.log(recommendation)
+    socket.broadcast.emit('newRecommendation', { recommendation })
+  })
+  socket.on('foo', bar => console.log(bar))
+  console.log('connected')
 })
 
 
