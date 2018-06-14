a994ec47e80afd53a90259726b68f003b1f4d39a
Binyamin Rosenbaum <biny235@users.noreply.github.com>
2018-06-13 21:31:41 -0400

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const _server = require('http').createServer(require('./server/app'));
 const port = process.env.PORT || 3000;
 const server = _server.listen(port, () => console.log(`Listening on port ${port}`));
 
-//process.env.SYNC = true
+// process.env.SYNC = true
 
 process.env.SYNC ? seed() : null;
 
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
   socket.emit('connected')
   socket.on('broadcasting', (plan)=> {
     console.log(plan)
-    socket.broadcast.emit('newBroadcast', plan )
+    socket.broadcast.emit('newBroadcast', plan );
   })
   socket.on('recommending', (recommendation)=> {
     console.log(recommendation)
